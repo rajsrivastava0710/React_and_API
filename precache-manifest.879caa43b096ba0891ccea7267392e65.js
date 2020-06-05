bd01303240d92bf884f4cbd609351b2387a4960b
Raj <rajsriv.14@gmail.com>
2020-06-05 23:12:38 +0530

    Updates

---
+++ precache-manifest.879caa43b096ba0891ccea7267392e65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc79fc9552cc7f8d4b2d0c42e9066d5",
+    "url": "/React_and_API/index.html"
+  },
+  {
+    "revision": "7e9a31153dd8d78a0411",
+    "url": "/React_and_API/static/css/main.b0e72dde.chunk.css"
+  },
+  {
+    "revision": "ef8c5d9f6c3057b96926",
+    "url": "/React_and_API/static/js/2.31fba054.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_and_API/static/js/2.31fba054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9a31153dd8d78a0411",
+    "url": "/React_and_API/static/js/main.e835ae59.chunk.js"
+  },
+  {
+    "revision": "845735ac812c87131857",
+    "url": "/React_and_API/static/js/runtime-main.e27459f4.js"
+  }
+]);
